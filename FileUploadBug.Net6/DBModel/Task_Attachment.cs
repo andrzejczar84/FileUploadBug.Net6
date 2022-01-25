f57b33f121c5a41ebd89fa92c1e062940f85aa5f
Andrzej Czarnecki <aczarna1@adient.com>
2022-01-25 19:09:14 +0100

    pdf upload failed

---
+++ FileUploadBug.Net6/DBModel/Task_Attachment.cs
@@ -0,0 +1,12 @@
+﻿namespace FileUploadBug.Net6.DBModel
+{
+    public class Task_Attachment
+    {
+        public int Id { get; set; }
+        public byte[] Attachment { get; set; }
+        public string Description { get; set; }
+        public DateTime Uploaded { get; set; }
+        public string UploadedBy { get; set; }
+        public string FileType { get; set; }
+    }
+}
--- FileUploadBug.Net6/Pages/IndexBase.cs
+++ FileUploadBug.Net6/Pages/IndexBase.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FileUploadBug.Net6.DBModel;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace FileUploadBug.Net6.Pages
@@ -10,20 +11,77 @@ namespace FileUploadBug.Net6.Pages
         public string imageDataUrl = string.Empty;
 
         public int readbytes;
+
+        /// <summary>
+        /// to repro img only
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        //protected async Task OnInputFileChange(InputFileChangeEventArgs e)
+        //{
+
+        //    var format = "image/png";
+
+        //    var imageFile = e.File;
+        //    var resizedImageFile = await imageFile.RequestImageFileAsync(format,
+        //            400, 200);
+        //    var buffer = new byte[resizedImageFile.Size];
+        //    readbytes = await resizedImageFile.OpenReadStream().ReadAsync(buffer);
+        //    imageDataUrl =
+        //        $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+
+        //    ImgUploaded = buffer;
+
+        //}
+
         protected async Task OnInputFileChange(InputFileChangeEventArgs e)
         {
 
-            var format = "image/png";
+            var format = e.File.ContentType.ToString();
+            
 
-            var imageFile = e.File;
-            var resizedImageFile = await imageFile.RequestImageFileAsync(format,
-                    400, 200);
-            var buffer = new byte[resizedImageFile.Size];
-            readbytes = await resizedImageFile.OpenReadStream().ReadAsync(buffer);
-            imageDataUrl =
-                $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+            if (format == "image/jpeg")
+            {
+                var imageFile = e.File;
+                var resizedImageFile = await imageFile.RequestImageFileAsync(format,
+                        600, 800);
+                var buffer = new byte[resizedImageFile.Size];
+                readbytes = await resizedImageFile.OpenReadStream().ReadAsync(buffer);
 
-            ImgUploaded = buffer;
+                Task_Attachment AddAttachment = new Task_Attachment()
+                {
+                    Attachment = buffer.ToArray(),
+                    Description = e.File.Name,
+                    Uploaded = DateTime.Now,
+                    FileType = e.File.ContentType
+                };
+                
+
+                //Db.Task_Attachments.Add(AddAttachment);
+                //Db.SaveChanges();
+            }
+            if (format == "application/pdf")
+            {
+                if (e.File.Size > 0 && e.File.Size < 2097152) //2 MB
+                {
+                    var buffer = new byte[e.File.Size];
+                    readbytes = await e.File.OpenReadStream(2097152).ReadAsync(buffer);
+                    
+                    Task_Attachment AddAttachment = new Task_Attachment()
+                    {
+                        Attachment = buffer.ToArray(),
+                        Description = e.File.Name,
+                        Uploaded = DateTime.Now,
+                        FileType = e.File.ContentType
+                    };
+                    //Db.Task_Attachments.Add(AddAttachment);
+                    //Db.SaveChanges();
+                }
+
+            }
+
+
+
 
         }
 
