3a5638b23ebc5bed3ca5880582cc2c42bb3d06b3
Andrzej Czarnecki <aczarna1@adient.com>
2022-01-25 16:58:31 +0100

    Dodaj pliki projektów.

---
+++ FileUploadBug.Net6/Pages/IndexBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace FileUploadBug.Net6.Pages
+{
+    public class IndexBase: ComponentBase
+    {
+        public byte[] ImgUploaded { get; set; }
+
+        public string imageDataUrl = string.Empty;
+
+        public int readbytes;
+        protected async Task OnInputFileChange(InputFileChangeEventArgs e)
+        {
+
+            var format = "image/png";
+
+            var imageFile = e.File;
+            var resizedImageFile = await imageFile.RequestImageFileAsync(format,
+                    400, 200);
+            var buffer = new byte[resizedImageFile.Size];
+            readbytes = await resizedImageFile.OpenReadStream().ReadAsync(buffer);
+            imageDataUrl =
+                $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+
+            ImgUploaded = buffer;
+
+        }
+
+    }
+}
